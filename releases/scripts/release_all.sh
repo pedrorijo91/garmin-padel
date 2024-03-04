6590f3677c3e4c6ccbe0dc9cd9ab568b5f3f2b47
pedrorijo91 <pedrorijo91@gmail.com>
2024-03-04 19:36:03 +0000

    final bits

--- releases/scripts/release_all.sh
+++ releases/scripts/release_all.sh
@@ -11,20 +11,20 @@ fi
 sh releases/scripts/export.sh
 
 version_filename=$(echo $new_version | sed 's/\./_/g')
-mv garminpadel.iq garminpadel-v$version_filename.iq
+mv releases/garminpadel.iq releases/garminpadel-v$version_filename.iq
 
-# git add resources/strings/strings.xml releases/
-# git cmsg "add v$new_version exported app"
-# git tag -a $new_version -m "version $new_version"
+git add resources/strings/strings.xml releases/
+git cmsg "add v$new_version exported app"
+git tag -a $new_version -m "version $new_version"
 
-# python replace_version.py next_beta
-# git add resources/strings/strings.xml
-# git cmsg "bump version for next development cycle"
+python replace_version.py next_beta
+git add resources/strings/strings.xml
+git cmsg "bump version for next development cycle"
 
-# git po && git ptags
+git po && git ptags
 
-# echo "Missing steps:"
-# echo "close milestone https://github.com/pedrorijo91/garmin-padel/milestones"
-# echo "create github release"
-# echo "add exported app to release"
-# echo "upload https://apps.garmin.com/en-US/developer/dashboard"
+echo "Missing steps:"
+echo "close milestone https://github.com/pedrorijo91/garmin-padel/milestones"
+echo "create github release"
+echo "add exported app to release"
+echo "upload https://apps.garmin.com/en-US/developer/dashboard"
