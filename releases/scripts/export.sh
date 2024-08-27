f8481fab308cd4ebaa148370dc92a57781ea8f91
pedrorijo91 <pedrorijo91@gmail.com>
2024-08-27 21:09:54 +0100

    add v1.11.0 exported app

--- releases/scripts/export.sh
+++ releases/scripts/export.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 echo "exporting app"
-java -Xms1g -Dfile.encoding=UTF-8 -Dapple.awt.UIElement=true -jar /Users/pedro/Library/Application\ Support/Garmin/ConnectIQ/Sdks/connectiq-sdk-mac-6.4.2-2024-01-04-a1dd13ee0/bin/monkeybrains.jar -o /Users/pedro/repos/garmin/garminpadel/releases/garminpadel.iq -f /Users/pedro/repos/garmin/garminpadel/monkey.jungle -y /Users/pedro/repos/garmin/developer_key -e -r -w 
+java -Xms1g -Dfile.encoding=UTF-8 -Dapple.awt.UIElement=true -jar /Users/pedro/Library/Application\ Support/Garmin/ConnectIQ/Sdks/connectiq-sdk-mac-7.3.0-2024-08-27-36488f6f5/bin/monkeybrains.jar -o /Users/pedro/repos/garmin/garminpadel/releases/garminpadel.iq -f /Users/pedro/repos/garmin/garminpadel/monkey.jungle -y /Users/pedro/repos/garmin/developer_key -e -r -w 
